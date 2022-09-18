a12f43818164e1df1a02fa2a75982d9b65df7bc7
Daniel Christian Mandolang <74773920+danielcm585@users.noreply.github.com>
2022-09-18 21:23:34 +0700

    Week02 assignment

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="CDFF4BA5A64738D9"
+REC2="94E55C6BC398082A"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
