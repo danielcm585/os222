859782b0ca12f22f8933f315fa8bb7f8304495d3
Daniel Christian Mandolang <danielcm585@gmail.com>
2022-09-22 23:13:22 +0700

    Fix debian update

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -12,7 +12,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="94E55C6BC398082A"
+REC2="88F30D44AD7F4C23"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
